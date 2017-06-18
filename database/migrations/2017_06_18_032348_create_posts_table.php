b8817e87ff032446382f8319068a78cf0929edd5
TanakaMana <makkuromyaon@gmail.com>
2017-06-18 13:13:30 +0900

    add column of posts table

--- database/migrations/2017_06_18_032348_create_posts_table.php
+++ database/migrations/2017_06_18_032348_create_posts_table.php
@@ -13,7 +13,12 @@ class CreatePostsTable extends Migration
      */
     public function up()
     {
-        //
+        Schema::create('posts', function (Blueprint $table) {
+            $table->increments('id');
+            $table->string('post');
+            $table->string('image');
+            $table->timestamps();
+        });
     }
 
     /**
@@ -23,6 +28,6 @@ class CreatePostsTable extends Migration
      */
     public function down()
     {
-        //
+        Schema::drop('posts');
     }
 }
