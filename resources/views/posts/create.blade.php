83adb3fecb0999f27752cb9bcef413ce49440dfa
TanakaMana <makkuromyaon@gmail.com>
2017-06-18 16:57:42 +0900

    add form of post

--- resources/views/posts/create.blade.php
+++ resources/views/posts/create.blade.php
@@ -1,1 +1,14 @@
-<p>新規作成</p>
+@extends('layout')
+
+@section('content')
+<div class= "contents row">
+  <div class= "container">
+    {{ Form::open(['url' => '/posts', 'method' => 'post']) }}
+      <h3>Post</h3>
+      <input placeholder="image url" type="text" name="image">
+      <textarea cols="30" rows="10" placeholder="text" type="text" name="post"></textarea>
+      <input type="submit" value="投稿する">
+    {{ Form::close() }}
+  </div>
+</div>
+@endsection
