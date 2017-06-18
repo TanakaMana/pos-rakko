3916fdd84e4e6882a9106f92bc1ed22ed504144f
TanakaMana <makkuromyaon@gmail.com>
2017-06-18 18:23:52 +0900

    set up CSS to be loaded on all blades

--- resources/views/layout.blade.php
+++ resources/views/layout.blade.php
@@ -5,7 +5,7 @@
     <title>ラッコのポスト</title>
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <!-- Bootstrap -->
-    <link href="css/bootstrap.min.css" rel="stylesheet" media="screen">
+    <link href="{{{asset('/css/bootstrap.min.css')}}}" rel="stylesheet" media="screen">
     <script src="http://code.jquery.com/jquery.js"></script>
     <script src="js/bootstrap.min.js"></script>
   </head>
