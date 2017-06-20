118d2de11fe41ff31caeb0b7aed5b0f422806ab6
TanakaMana <makkuromyaon@gmail.com>
2017-06-20 20:28:37 +0900

    modify css directory

--- resources/views/layout.blade.php
+++ resources/views/layout.blade.php
@@ -5,8 +5,7 @@
     <title>ラッコのポスト</title>
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <!-- Bootstrap -->
-    <link href="{{{asset('/sass/base.scss')}}}" rel="stylesheet" media="screen">
-    <link href="{{{asset('/css/custom.css')}}}" rel="stylesheet" media="screen">
+    <link href="{{{asset('/css/base.css')}}}" rel="stylesheet">
     <script src="http://code.jquery.com/jquery.js"></script>
     <script src="js/bootstrap.min.js"></script>
   </head>
@@ -16,12 +15,8 @@
       <nav class="navbar navbar-default navbar-fixed-top">
         <div class="container">
           <div class="navbar-header">
-            <button type="button" class="navbar-toggle collapsed" data-toggle="collapse" data-target="#navbar" aria-expanded="false" aria-controls="navbar">
-              <span class="icon-bar"></span>
-              <span class="icon-bar"></span>
-              <span class="icon-bar"></span>
-            </button>
-            <a class="navbar-brand" href="/">ラッコのポスト</a>
+            <a class="title" href="/">ラッコのポスト</a>
+            <a class="post" href="/posts/create">投稿する</a>
           </div>
         </div>
       </nav>
