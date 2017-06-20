55a42be3ff1a5bd8a687d68640c9155ea84cfc63
TanakaMana <makkuromyaon@gmail.com>
2017-06-20 21:20:06 +0900

    add japanese message

---
+++ resources/lang/ja/validation.php
@@ -0,0 +1,15 @@
+<!-- 'costom' => [
+  'attribute-name' => [
+    'rule-name' => 'custom-message'.
+    ],
+    'email' => [
+      'required' => ':attributeを入力してください',
+    ],
+    'password' => [
+      'required' => ':attributeを入力してください',
+    ],
+
+    'attributes' => [
+      'email' => 'メールアドレス',
+      'password' => 'パスワード'],
+    ], -->
