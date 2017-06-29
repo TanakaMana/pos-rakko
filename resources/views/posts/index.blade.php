01db678b2a8bee7ea4bb864a381d139cbeef166e
TanakaMana <makkuromyaon@gmail.com>
2017-06-29 19:47:59 +0900

    modify css style name

--- resources/views/posts/index.blade.php
+++ resources/views/posts/index.blade.php
@@ -1,8 +1,8 @@
 @extends('layout')
 
 @section('content')
-  @foreach($posts as $post)
-    <div class="rakko_post" style="background-image: url({{ $post->image }});">
+  @foreach ($posts as $post)
+    <div class="rakkoPost" style="background-image: url({{ $post->image }});">
       <p>{{ $post->post }}</p>
     </div>
   @endforeach
