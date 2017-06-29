bb200dd348f7379a272e1ded0f41c562ab8ab52c
TanakaMana <makkuromyaon@gmail.com>
2017-06-29 19:46:24 +0900

    set association

--- app/Post.php
+++ app/Post.php
@@ -6,5 +6,12 @@ use Illuminate\Database\Eloquent\Model;
 
 class Post extends Model
 {
-    protected $fillable = array('image', 'post');
+    protected $fillable = [
+        'image', 'post', 'user_id'
+    ];
+
+    public function post()
+    {
+        return $this->belongsTo('User');
+    }
 }
