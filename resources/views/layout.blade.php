50675c7ee6851aa63112f58c3b019bc0a21266ae
TanakaMana <makkuromyaon@gmail.com>
2017-06-29 15:47:36 +0900

    change title ポスト to ぽすと

--- resources/views/layout.blade.php
+++ resources/views/layout.blade.php
@@ -7,7 +7,7 @@
     <!-- CSRF Token -->
     <meta name="csrf-token" content="{{ csrf_token() }}">
 
-    <title>ラッコのポスト</title>
+    <title>ラッコのぽすと</title>
 
     <!-- Bootstrap -->
     <link href="{{ asset('css/app.css') }}" rel="stylesheet">
@@ -31,7 +31,7 @@
 
                       <!-- Branding Image -->
                       <a class="navbar-brand" href="{{ url('/') }}">
-                          ラッコのポスト
+                          ラッコのぽすと
                       </a>
                   </div>
 
--- resources/views/posts/create.blade.php
+++ resources/views/posts/create.blade.php
@@ -5,7 +5,7 @@
   <div class= "container">
     <div class= "form-group">
       {{ Form::open(['url' => '/posts', 'method' => 'post']) }}
-        <h3>ポスト</h3>
+        <h3>ぽすとする</h3>
         <input placeholder="image url" type="text" name="image" class="form-control">
         <textarea cols="30" rows="10" placeholder="text" type="text" name="post" class="form-control"></textarea>
         <input type="submit" value="投稿する" class="btn btn-default">
