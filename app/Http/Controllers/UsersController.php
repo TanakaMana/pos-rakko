d07775980e5a820402f550dee7ba5c9dc90259aa
TanakaMana <makkuromyaon@gmail.com>
2017-06-30 23:31:53 +0900

    add show action in usersController.php

--- app/Http/Controllers/UsersController.php
+++ app/Http/Controllers/UsersController.php
@@ -4,7 +4,21 @@ namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
 
+use App\Http\Requests;
+use Auth;
+use App\Post;
+
 class UsersController extends Controller
 {
-    //
+    public function show()
+    {
+        $name = Auth::user()->name;
+        $posts = Post::where('user_id', Auth::user()->id);
+
+        return view('users.show')->with(array('name' => $name, 'posts' => $posts));
+    }
+
+    public function edit()
+    {
+    }
 }
